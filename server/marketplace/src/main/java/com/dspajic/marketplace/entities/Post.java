f3011a76f8c433f282e351acb6a4b9eea4e13e98
spajicdominik <dominikspajic7@gmail.com>
2026-01-27 22:12:32 +0100

    Created entity class for posts

---
+++ server/marketplace/src/main/java/com/dspajic/marketplace/entities/Post.java
@@ -0,0 +1,112 @@
+package com.dspajic.marketplace.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "user_id")
+    private int userID;
+
+    @Column(name = "product_id")
+    private int productID;
+
+    public Post() {
+    }
+
+    public Post(String title, int price, int productID, String currency, int userID, String description) {
+        this.title = title;
+        this.price = price;
+        this.productID = productID;
+        this.currency = currency;
+        this.userID = userID;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", currency='" + currency + '\'' +
+                ", userID=" + userID +
+                ", productID=" + productID +
+                '}';
+    }
+}
