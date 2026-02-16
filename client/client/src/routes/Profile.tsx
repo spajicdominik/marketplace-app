338f9aa2858b927ed82bbc8637ba5edb2ae692e4
Dominik Spajic <dominikspajic7@gmail.com>
2026-02-16 16:03:54 +0100

    started dev profile

--- client/client/src/routes/Profile.tsx
+++ client/client/src/routes/Profile.tsx
@@ -1,5 +1,19 @@
+import { useParams } from "react-router-dom"
+import useFetchUserDetails from "../hooks/userDetails/useFetchUserDetails";
+import UserInfo from "../features/userInfo/UserInfo";
+import UserPosts from "../features/userInfo/UserPosts";
+import { useSelector } from "react-redux";
+import type { RootState } from "../store";
+import ProfileMenu from "../features/profile/sidebar/ProfileMenu";
+
 export default function Profile() {
+    const userId = useSelector((state : RootState ) => state.auth.user?.user_id);
+    const userDetails = useFetchUserDetails(userId);
+
     return (
-        <div></div>
+        <div className="flex">
+            <ProfileMenu></ProfileMenu>
+            <UserInfo userDetails={userDetails}/>
+        </div>
     )
 }
