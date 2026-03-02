99810f96db802df273a8029de64cdb37e269599a
Dominik Spajic <dominikspajic7@gmail.com>
2026-03-02 14:15:28 +0100

    styles

--- client/client/src/routes/Root.tsx
+++ client/client/src/routes/Root.tsx
@@ -3,9 +3,9 @@ import FooterPage from "../features/footer/FooterPage";
 import { Outlet } from "react-router-dom";
 
 
-export default function RootLayout() {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div>
+        <div className="min-h-screen flex flex-col">
         <Header></Header>
         <Outlet></Outlet>
         <FooterPage></FooterPage>
